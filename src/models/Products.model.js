fd59ca7ce90ddc7a96bd1ac1553f1d3572483825
Your Name <you@example.com>
2023-11-20 10:53:27 -0300

    se agg item color

--- src/models/Products.model.js
+++ src/models/Products.model.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const productsSchema = new mongoose.Schema({
     modelo: { type: String },
     precio: { type: String },
-    tallas: { type: String },
-    imagen: { type: String}
+    color: { type: String },
+    imagen: { type: String }
 })
 
 const Products = mongoose.model('Products', productsSchema)
